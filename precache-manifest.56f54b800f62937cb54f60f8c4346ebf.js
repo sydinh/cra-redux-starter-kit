9b97dcd70c9abe2a06b68c51509e7df084de9274
sydinh <sydinh.dev@gmail.com>
2019-01-07 13:17:14 +0700

    Updates

---
+++ precache-manifest.56f54b800f62937cb54f60f8c4346ebf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f5531f3bbef4a62168d",
+    "url": "/cra-redux-starter-kit/static/js/0.1f5531f3.chunk.js"
+  },
+  {
+    "revision": "e3c7e16cc38d375a1564",
+    "url": "/cra-redux-starter-kit/static/css/main.7eacdd01.chunk.css"
+  },
+  {
+    "revision": "e3c7e16cc38d375a1564",
+    "url": "/cra-redux-starter-kit/static/js/main.e3c7e16c.chunk.js"
+  },
+  {
+    "revision": "e837ef9daa85582563c5",
+    "url": "/cra-redux-starter-kit/static/js/2.e837ef9d.chunk.js"
+  },
+  {
+    "revision": "e7557950dc822691c7b0",
+    "url": "/cra-redux-starter-kit/static/js/3.e7557950.chunk.js"
+  },
+  {
+    "revision": "ad16560506629698535c",
+    "url": "/cra-redux-starter-kit/static/js/4.ad165605.chunk.js"
+  },
+  {
+    "revision": "ac6ea25b2cea4fceb28a",
+    "url": "/cra-redux-starter-kit/static/js/5.ac6ea25b.chunk.js"
+  },
+  {
+    "revision": "a85ffd61307f6afa8278",
+    "url": "/cra-redux-starter-kit/static/js/6.a85ffd61.chunk.js"
+  },
+  {
+    "revision": "d9d9394908706db78aa9",
+    "url": "/cra-redux-starter-kit/static/js/7.d9d93949.chunk.js"
+  },
+  {
+    "revision": "ada8692c182268940ef1",
+    "url": "/cra-redux-starter-kit/static/js/runtime~main.ada8692c.js"
+  },
+  {
+    "revision": "4c1343da8a7522e159758e7804d9bccc",
+    "url": "/cra-redux-starter-kit/index.html"
+  }
+];
