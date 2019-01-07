6c76519a3e4968edc7f7617e06640a9a8a72607d
sydinh <sydinh.dev@gmail.com>
2019-01-07 14:49:52 +0700

    add shared stuff

---
+++ src/components/Shared/Button.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Button = () => <button type="button">button</button>;
+
+export default Button;
---
+++ src/components/Shared/Input.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Input = () => <input type="text" placeholder="placeholder" />;
+
+export default Input;
